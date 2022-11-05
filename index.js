a23b2d668d7d3d9cc033a14ae3743d668d421c9c
Victor Ratts <victor@roverlabs.com.br>
2022-11-05 11:05:06 -0300

    update version 1.0.8

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // index.js
-// const fetch = require('axios-fetch-request');
-// const dateFormat = require('dateformat');
 import dateFormat from "dateformat";
 import fetch from "axios-fetch-request";
 const api = new fetch();
@@ -21,7 +19,7 @@ class Score {
     getlang(lang = false) {
         return new Promise((resolve, reject) => {
             //https://api.sofascore.com/api/v1/country/alpha2
-            api.get(`${base}/api/${this.version || 'v1'}/country/alpha2`, false, false).then(rest => {
+            api.get(`${base}/api/${this.version || 'v1'}/country/alpha2`).then(rest => {
                 return resolve(rest)
             }).catch(e => {
                 return reject(e)
@@ -32,7 +30,7 @@ class Score {
     eventCount(count = '-10800') {
         return new Promise((resolve, reject) => {
             //https://api.sofascore.com/api/v1/sport/-10800/event-count
-            api.get(`${base}/api/${this.version || 'v1'}/sport/${count}/event-count`, false, false).then(rest => {
+            api.get(`${base}/api/${this.version || 'v1'}/sport/${count}/event-count`).then(rest => {
                 return resolve(rest)
             }).catch(e => {
                 return reject(e)
@@ -43,7 +41,7 @@ class Score {
     scheduledEvents(dateEvent = dateFormat(this.now, 'yyyy-mm-dd')) {
         return new Promise((resolve, reject) => {
             //https://api.sofascore.com/api/v1/sport/${this.sport}/scheduled-events/2021-07-12
-            api.get(`${base}/api/${this.version || 'v1'}/sport/${this.sport}/scheduled-events/${dateEvent}`, false, false).then(rest => {
+            api.get(`${base}/api/${this.version || 'v1'}/sport/${this.sport}/scheduled-events/${dateEvent}`).then(rest => {
                 return resolve(rest)
             }).catch(e => {
                 return reject(e)
@@ -54,7 +52,7 @@ class Score {
     oddsProviders(lang = 'PT') {
         return new Promise((resolve, reject) => {
             //https://api.sofascore.com/api/v1/odds/providers/BR/web
-            api.get(`${base}/api/${this.version || 'v1'}/odds/providers/${lang}/web`, false, false).then(rest => {
+            api.get(`${base}/api/${this.version || 'v1'}/odds/providers/${lang}/web`).then(rest => {
                 return resolve(rest)
             }).catch(e => {
                 return reject(e)
@@ -66,7 +64,7 @@ class Score {
         uniqueTournamentsWeb(lang = 'PT') {
             return new Promise((resolve, reject) => {
                 //https://api.sofascore.com/api/v1/config/unique-tournaments/BR
-                api.get(`${base}/api/${this.version || 'v1'}/config/unique-tournaments/${lang}`, false, false).then(rest => {
+                api.get(`${base}/api/${this.version || 'v1'}/config/unique-tournaments/${lang}`).then(rest => {
                     return resolve(rest)
                 }).catch(e => {
                     return reject(e)
@@ -76,7 +74,7 @@ class Score {
         uniqueTournamentsFootball(lang = 'PT') {
             return new Promise((resolve, reject) => {
                 //https://api.sofascore.com/api/v1/config/unique-tournaments/BR/football
-                api.get(`${base}/api/${this.version || 'v1'}/config/unique-tournaments/${lang}/football`, false, false).then(rest => {
+                api.get(`${base}/api/${this.version || 'v1'}/config/unique-tournaments/${lang}/football`).then(rest => {
                     return resolve(rest)
                 }).catch(e => {
                     return reject(e)
@@ -89,7 +87,7 @@ class Score {
     categories() {
         return new Promise((resolve, reject) => {
             //https://api.sofascore.com/api/v1/sport/${this.sport}/categories
-            api.get(`${base}/api/${this.version || 'v1'}/sport/${this.sport}/categories`, false, false).then(rest => {
+            api.get(`${base}/api/${this.version || 'v1'}/sport/${this.sport}/categories`).then(rest => {
                 return resolve(rest)
             }).catch(e => {
                 return reject(e)
@@ -100,7 +98,7 @@ class Score {
     toDay(dateEvent = dateFormat(this.now, 'yyyy-mm-dd'), count = '-10800') {
         return new Promise((resolve, reject) => {
             //https://api.sofascore.com/api/v1/sport/${this.sport}/2021-07-12/-10800/categories
-            api.get(`${base}/api/${this.version || 'v1'}/sport/${this.sport}/${dateEvent}/${count}/categories`, false, false).then(rest => {
+            api.get(`${base}/api/${this.version || 'v1'}/sport/${this.sport}/${dateEvent}/${count}/categories`).then(rest => {
                 return resolve(rest)
             }).catch(e => {
                 return reject(e)
@@ -111,7 +109,7 @@ class Score {
     trendingTopPlayers() {
         return new Promise((resolve, reject) => {
             //https://api.sofascore.com/api/v1/sport/${this.sport}/trending-top-players
-            api.get(`${base}/api/${this.version || 'v1'}/sport/${this.sport}/trending-top-players`, false, false).then(rest => {
+            api.get(`${base}/api/${this.version || 'v1'}/sport/${this.sport}/trending-top-players`).then(rest => {
                 return resolve(rest)
             }).catch(e => {
                 return reject(e)
@@ -122,7 +120,7 @@ class Score {
     oddsFeaturedEvents(dateEvent = dateFormat(this.now, 'yyyy-mm-dd'), page = 1, rand = '-10800') {
         return new Promise((resolve, reject) => {
             //https://api.sofascore.com/api/v1/odds/1/featured-events/${this.sport}/2021-07-12/-10800
-            api.get(`${base}/api/${this.version || 'v1'}/odds/${page}/featured-events/${this.sport}/${dateEvent}/${rand}`, false, false).then(rest => {
+            api.get(`${base}/api/${this.version || 'v1'}/odds/${page}/featured-events/${this.sport}/${dateEvent}/${rand}`).then(rest => {
                 return resolve(rest)
             }).catch(e => {
                 return reject(e)
@@ -133,7 +131,7 @@ class Score {
     fracionalOdds(event = '', page = 1) {
         return new Promise((resolve, reject) => {
             //https://api.sofascore.com/api/v1/event/9223865/odds/1/featured
-            api.get(`${base}/api/${this.version || 'v1'}/event/${event}/odds/${page}/featured`, false, false).then(rest => {
+            api.get(`${base}/api/${this.version || 'v1'}/event/${event}/odds/${page}/featured`).then(rest => {
                 return resolve(rest)
             }).catch(e => {
                 return reject(e)
@@ -144,7 +142,7 @@ class Score {
     FootballFeaturedEvents(page) {
         return new Promise((resolve, reject) => {
             //https://api.sofascore.com/api/v1/odds/1/featured-events/football
-            api.get(`${base}/api/${this.version || 'v1'}/odds/${page}/featured-events/football`, false, false).then(rest => {
+            api.get(`${base}/api/${this.version || 'v1'}/odds/${page}/featured-events/football`).then(rest => {
                 return resolve(rest)
             }).catch(e => {
                 return reject(e)
@@ -155,7 +153,7 @@ class Score {
     heatMap(event = '', player = '') {
         return new Promise((resolve, reject) => {
             //https://api.sofascore.com/api/v1/event/9506640/player/309464/heatmap
-            api.get(`${base}/api/${this.version || 'v1'}/event/${event}/player/${player}/heatmap`, false, false).then(rest => {
+            api.get(`${base}/api/${this.version || 'v1'}/event/${event}/player/${player}/heatmap`).then(rest => {
                 return resolve(rest)
             }).catch(e => {
                 return reject(e)
@@ -166,7 +164,7 @@ class Score {
     featuredOdds(event = '', page = 1) {
         return new Promise((resolve, reject) => {
             //https://api.sofascore.com/api/v1/event/9286604/odds/1/featured
-            api.get(`${base}/api/${this.version || 'v1'}/event/${event}/odds/${page}/featured`, false, false).then(rest => {
+            api.get(`${base}/api/${this.version || 'v1'}/event/${event}/odds/${page}/featured`).then(rest => {
                 return resolve(rest)
             }).catch(e => {
                 return reject(e)
@@ -177,7 +175,7 @@ class Score {
     allData(event = '', page = 1) {
         return new Promise((resolve, reject) => {
             //https://api.sofascore.com/api/v1/event/9476094/odds/1/all
-            api.get(`${base}/api/${this.version || 'v1'}/event/${event}/odds/${page}/all`, false, false).then(rest => {
+            api.get(`${base}/api/${this.version || 'v1'}/event/${event}/odds/${page}/all`).then(rest => {
                 return resolve(rest)
             }).catch(e => {
                 return reject(e)
@@ -188,7 +186,7 @@ class Score {
     votes(event = '') {
         return new Promise((resolve, reject) => {
             //https://api.sofascore.com/api/v1/event/9223864/votes
-            api.get(`${base}/api/${this.version || 'v1'}/event/${event}/votes`, false, false).then(rest => {
+            api.get(`${base}/api/${this.version || 'v1'}/event/${event}/votes`).then(rest => {
                 return resolve(rest)
             }).catch(e => {
                 return reject(e)
@@ -199,7 +197,7 @@ class Score {
     statistics(event = '') {
         return new Promise((resolve, reject) => {
             //https://api.sofascore.com/api/v1/event/9223864/statistics
-            api.get(`${base}/api/${this.version || 'v1'}/event/${event}/statistics`, false, false).then(rest => {
+            api.get(`${base}/api/${this.version || 'v1'}/event/${event}/statistics`).then(rest => {
                 return resolve(rest)
             }).catch(e => {
                 return reject(e)
@@ -210,7 +208,7 @@ class Score {
     liveEvents() {
         return new Promise((resolve, reject) => {
             //https://api.sofascore.com/api/v1/sport/football/events/live
-            api.get(`${base}/api/v1/sport/${this.sport}/events/live`, false, false).then(rest => {
+            api.get(`${base}/api/v1/sport/${this.sport}/events/live`).then(rest => {
                 return resolve(rest)
             }).catch(e => {
                 return reject(e)
@@ -220,7 +218,7 @@ class Score {
 
     openUrl(url) {
         return new Promise((resolve, reject) => {
-            api.get(url, false, false).then(rest => {
+            api.get(url).then(rest => {
                 return resolve(rest)
             }).catch(e => {
                 return reject(e)
